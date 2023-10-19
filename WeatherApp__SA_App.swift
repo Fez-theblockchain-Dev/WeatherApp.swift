bc7901fe86da1e08e5f9aaeff9c880794cfc519c
Ramez Festek <108371436+Fez-theblockchain-Dev@users.noreply.github.com>
2023-10-19 13:00:46 -0400

    added @main file and custom color

---
+++ WeatherApp__SA_App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp__SA_App.swift
+//  WeatherApp_(SA)
+//
+//  Created by Ramez on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp__SA_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
